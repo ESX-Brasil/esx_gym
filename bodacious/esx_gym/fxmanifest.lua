08ce1be4b42de3b3615dd3f5d463c190e6dbfbfe
renildomarcio <krsolucoesweb@gmail.com>
2022-12-05 01:00:37 -0300

    Update 05/12/2022
    FX Version:
    * adamant
    * bodacious
    * cerulean

---
+++ bodacious/esx_gym/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'ESX Gym'
+
+version '0.0.1'
+
+server_scripts {
+  '@es_extended/locale.lua',
+  '@mysql-async/lib/MySQL.lua',
+  'server/main.lua'
+}
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'client/main.lua'
+}
